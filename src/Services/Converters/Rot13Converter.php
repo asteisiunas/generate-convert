fabcdccec4882cce336684551d5b0e70d4c614b9
Duadus <info@duadus.shop>
2024-03-23 14:50:14 +0200

    Rot13Converter

---
+++ src/Services/Converters/Rot13Converter.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Converters;
+
+class Rot13Converter implements ConverterInterface
+{
+    public function convert(string $input): string
+    {
+        return str_rot13($input);
+    }
+}
